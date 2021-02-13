48bfcb545cc7c00caaf9fcf931ccacef22dad5a4
anasberrada <anasberrada@192.168.2.23>
2021-02-13 16:26:15 -0500

    Component added  (working version)
    --panel added
    --buttons added
    --textfield added
    TODO user the right layout make textfield wider

--- Calculator (learning)/src/App.java
+++ Calculator (learning)/src/App.java
@@ -4,14 +4,11 @@ import view.Gui;
 
 public class App {
 
-	public static void main(String[] args) {
-		
-//		String m= new String("Anas");		
-//		int age = 17;
-//		System.out.println("my name is :"+m +" I'm "+age);
+	public static void main(String[] args) {		
+
 		
 		 Gui myGui =   new Gui();
-		 myGui.setTitle("Calculator");
+
 		 
 		
 		
--- Calculator (learning)/src/view/Gui.java
+++ Calculator (learning)/src/view/Gui.java
@@ -1,6 +1,12 @@
 package view;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Gui extends JFrame {
 	
@@ -10,22 +16,66 @@ public class Gui extends JFrame {
 	//constructor
 	public Gui() {
 		
-		this.setSize(200, 200);
+		this.setSize(300, 200);
 		this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		//test git
+		this.setTitle("Calculator");
+
+		JTextField textfield = new JTextField();
+		textfield.setSize(300, 20);
+		textfield.setText("Testing");
+		JButton button1 = new JButton("1");
+		button1.setSize(50, 50);
+		
+		JButton button2 = new JButton("2");
+		button1.setSize(50, 50);
+		
+		JButton button3 = new JButton("3");
+		button1.setSize(50, 50);
+		
+		JButton button4 = new JButton("4");
+		button1.setSize(50, 50);
+		
+		JButton button5 = new JButton("5");
+		button1.setSize(50, 50);
+		
+		JButton button6 = new JButton("6");
+		button1.setSize(50, 50);
+		
+		JButton button7 = new JButton("7");
+		button1.setSize(50, 50);
+		
+		JButton button8 = new JButton("8");
+		button1.setSize(50, 50);
+		
+		JButton button9 = new JButton("9");
+		button1.setSize(50, 50);
+		
+		JButton button0 = new JButton("0");
+		button1.setSize(50, 50);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		
+		panel.add(textfield);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
+		panel.add(button6);
+		panel.add(button7);
+		panel.add(button8);
+		panel.add(button9);
+		panel.add(button0);
+		
+		
+		this.add(panel);
+		this.setVisible(true);
+		this.setResizable(false);
+		//this.pack();
 	}
 	
-	//constructor
-		public Gui(String s) {
-			
-			JFrame f = new JFrame (s);
-			f.setSize(200, 200);
-			f.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-			f.setVisible(true);
-			//bla
-		}
-		
-		//
+	
 	
      
 }
