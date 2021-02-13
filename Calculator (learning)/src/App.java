c766e68ee3a45e2d4fcdcf457ae89bbc60b2e9b3
anasberrada <anasberrada@192.168.2.23>
2021-02-13 14:41:00 -0500

    first commit of my project

---
+++ Calculator (learning)/src/App.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+
+import view.Gui;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+//		String m= new String("Anas");		
+//		int age = 17;
+//		System.out.println("my name is :"+m +" I'm "+age);
+		
+		 Gui myGui =   new Gui();
+		 myGui.setTitle("Calculator");
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Calculator (learning)/src/view/Gui.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Gui extends JFrame {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	//constructor
+	public Gui() {
+		
+		this.setSize(200, 200);
+		this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+	}
+	
+	//constructor
+		public Gui(String s) {
+			
+			JFrame f = new JFrame (s);
+			f.setSize(200, 200);
+			f.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+			f.setVisible(true);
+		}
+	
+     
+}
